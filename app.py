59605ae4518e3eac1c9d50118a1258ae4867f243
calli9 <3592330768@qq.com>
2025-11-04 20:33:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template
+
+load_dotenv() 
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY', 'fallback-local-key')
 
 @app.route('/')
+
 def home():
     return render_template('index.html')
 
